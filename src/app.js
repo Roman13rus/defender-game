ab72475c36430c11cb35efbde4b58cb21d301428
Roman Rogschev <omuch13rus@rambler.ru>
2024-01-18 22:03:47 +0200

    initional commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// const characters = [
+//     {name: 'мечник', health: 10},
+//     {name: 'маг', health: 100},
+//     {name: 'маг', health: 0},
+//     {name: 'лучник', health: 0},
+//   ];
+  
+// const alive = characters.filter(item => item.health > 0);
+
+// закомментированиы из-за ошибки линтера(объявлены, но не используются)
